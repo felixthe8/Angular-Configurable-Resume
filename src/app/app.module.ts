f4fd12de1a886ce4d1dababd48b2fa1d071775f4
Felix H <felixthe8@gmail.com>
2021-03-15 19:11:15 -0700

    Code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { MatListModule } from '@angular/material/list';
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatIconModule,
     MatDividerModule,
     MatListModule
   ],
