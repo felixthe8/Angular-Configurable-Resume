bb72b4caf49565b419c413ce28fc236cbe530ae0
Felix H <felixthe8@gmail.com>
2021-03-15 19:08:28 -0700

    Create header-component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  firstName = environment.name.firstName;
+  lastName = environment.name.lastName;
+  phoneNumber = environment.contact.phone;
+  email = environment.contact.email;
+  linkedin = environment.contact.linkedin;
+
+  ngOnInit(): void {
+  }
+
+}
